5a82f68e6df31a0f9084762196cb54f13e58e19a
clawlynx <clawlynx77@gmail.com>
2026-05-06 11:20:47 +0530

    added new dashboard stats

--- src/components/Dashboard/RevenueChart.jsx
+++ src/components/Dashboard/RevenueChart.jsx
@@ -1,48 +1,56 @@
-import * as React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
-import { LineChart } from '@mui/x-charts/LineChart';
+import * as React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+import { LineChart } from "@mui/x-charts/LineChart";
+import { useGetRevenueChart } from "../../hooks/stats/useStats";
 
 const COLORS = {
-  gold: '#C9A227',
-  lightBg: '#F5F5F4',
+  gold: "#C9A227",
+  lightBg: "#F5F5F4",
 };
 
 const RevenueChart = () => {
   const data = [4000, 3000, 2000, 2800, 1900, 2400, 3500];
+  const revenue = useGetRevenueChart();
 
   return (
-    <Card
-      sx={{
-        borderRadius: 3,
-        boxShadow: 3,
-        backgroundColor: COLORS.lightBg,
-        p: 2,
-      }}
-    >
-      <CardContent>
-        <Typography variant='h6' fontWeight={600} mb={2}>
-          Revenue Overview
-        </Typography>
+    !revenue.isLoading && (
+      <Card
+        sx={{
+          borderRadius: 3,
+          boxShadow: 3,
+          backgroundColor: COLORS.lightBg,
+          p: 2,
+        }}
+      >
+        <CardContent>
+          <Typography variant="h6" fontWeight={600} mb={2}>
+            Revenue Overview
+          </Typography>
 
-        <LineChart
-          height={300}
-          series={[
-            {
-              data: data,
-              color: COLORS.gold,
-              area: false,
-            },
-          ]}
-          xAxis={[
-            {
-              scaleType: 'point',
-              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            },
-          ]}
-          grid={{ vertical: false, horizontal: true }}
-        />
-      </CardContent>
-    </Card>
+          <LineChart
+            height={300}
+            series={[
+              {
+                data: revenue.data?.revenueChart?.map(
+                  (item) => item.revenue,
+                ) || [0, 0, 0, 0],
+                color: COLORS.gold,
+                area: false,
+              },
+            ]}
+            xAxis={[
+              {
+                scaleType: "point",
+                data: revenue.data?.revenueChart?.map((item) => item.date) || [
+                  0, 0, 0, 0,
+                ],
+              },
+            ]}
+            grid={{ vertical: false, horizontal: true }}
+          />
+        </CardContent>
+      </Card>
+    )
   );
 };
 
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Box, Container } from '@mui/material';
-import DashboardStats from '../../components/Dashboard/DashboardStats';
-import RevenueChart from '../../components/Dashboard/RevenueChart';
-import SalesCategory from '../../components/Dashboard/SalesCategory';
+import React from "react";
+import { Box, Container } from "@mui/material";
+import DashboardStats from "../../components/Dashboard/DashboardStats";
+import RevenueChart from "../../components/Dashboard/RevenueChart";
+import SalesCategory from "../../components/Dashboard/SalesCategory";
+import {
+  useGetCategoryStats,
+  useGetCustomerGrowth,
+  useGetDashboardOverview,
+  useGetRevenueChart,
+  useGetTopProducts,
+} from "../../hooks/stats/useStats";
 
 const Dashboard = () => {
+  // const topProducts = useGetTopProducts();
+  // const customerGrowth = useGetCustomerGrowth();
+  // const categoryStats = useGetCategoryStats();
   return (
     <Box
       sx={{
-        minHeight: '100vh',
-        backgroundColor: '#E7E5E4',
+        minHeight: "100vh",
+        backgroundColor: "#E7E5E4",
         py: 4,
       }}
     >
-      <Container maxWidth='xl'>
+      <Container maxWidth="xl">
         <Box mb={4}>
           <DashboardStats />
         </Box>
 
         <Box
-          display='grid'
-          gridTemplateColumns={{ xs: '1fr', md: '2fr 1fr' }}
+          display="grid"
+          gridTemplateColumns={{ xs: "1fr", md: "2fr 1fr" }}
           gap={3}
         >
           <RevenueChart />
